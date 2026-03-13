fbf8499cdb20fbbf2649832d4778d4ad812400be
MiguelOquendoRincon <oquendorinconmiguelangel@gmail.com>
2026-03-13 12:34:42 -0500

    feat(auth): define AuthState and AuthEvent with freezed

---
+++ lib/features/user_auth/presentation/bloc/auth_event.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'auth_event.freezed.dart';
+
+@freezed
+class AuthEvent with _$AuthEvent {
+  const factory AuthEvent.loginRequested({
+    required String email,
+    required String password,
+  }) = _LoginRequested;
+
+  const factory AuthEvent.logoutRequested() = _LogoutRequested;
+}
