835f26de54ddd879b8d4711c1920e0cddad27ee9
MiguelOquendoRincon <oquendorinconmiguelangel@gmail.com>
2026-03-13 12:22:49 -0500

    feat(auth): create UserModel with freezed

---
+++ lib/features/user_auth/data/models/user_model.dart
@@ -0,0 +1,22 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import '../../domain/entities/user_entity.dart';
+
+part 'user_model.freezed.dart';
+part 'user_model.g.dart';
+
+@freezed
+class UserModel with _$UserModel {
+  const factory UserModel({
+    required String id,
+    required String email,
+    String? name,
+  }) = _UserModel;
+
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
+
+  // Método de extensión para convertir Modelo a Entidad
+  // Esto mantiene el Dominio puro
+  const UserModel._();
+  UserEntity toEntity() => UserEntity(id: id, email: email, name: name);
+}
--- lib/injection/injection.config.dart
+++ lib/injection/injection.config.dart
@@ -12,13 +12,20 @@
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 
+import '../features/user_auth/domain/repositories/auth_repository.dart'
+    as _i425;
+import '../features/user_auth/domain/usecases/login_usecase.dart' as _i838;
+
 extension GetItInjectableX on _i174.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
   _i174.GetIt init({
     String? environment,
     _i526.EnvironmentFilter? environmentFilter,
   }) {
-    _i526.GetItHelper(this, environment, environmentFilter);
+    final gh = _i526.GetItHelper(this, environment, environmentFilter);
+    gh.lazySingleton<_i838.LoginUseCase>(
+      () => _i838.LoginUseCase(gh<_i425.AuthRepository>()),
+    );
     return this;
   }
 }
