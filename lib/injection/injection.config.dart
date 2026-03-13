8f1f4dce407b63e9547c322e560cf1ffac3eae1d
MiguelOquendoRincon <oquendorinconmiguelangel@gmail.com>
2026-03-13 12:33:00 -0500

    fix: Freezed class generated

--- lib/injection/injection.config.dart
+++ lib/injection/injection.config.dart
@@ -12,6 +12,10 @@
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 
+import '../features/user_auth/data/datasources/auth_remote_data_source.dart'
+    as _i188;
+import '../features/user_auth/data/repositories/auth_repository_impl.dart'
+    as _i925;
 import '../features/user_auth/domain/repositories/auth_repository.dart'
     as _i425;
 import '../features/user_auth/domain/usecases/login_usecase.dart' as _i838;
@@ -23,6 +27,12 @@ extension GetItInjectableX on _i174.GetIt {
     _i526.EnvironmentFilter? environmentFilter,
   }) {
     final gh = _i526.GetItHelper(this, environment, environmentFilter);
+    gh.lazySingleton<_i188.AuthRemoteDataSource>(
+      () => _i188.AuthRemoteDataSourceImpl(),
+    );
+    gh.lazySingleton<_i425.AuthRepository>(
+      () => _i925.AuthRepositoryImpl(gh<_i188.AuthRemoteDataSource>()),
+    );
     gh.lazySingleton<_i838.LoginUseCase>(
       () => _i838.LoginUseCase(gh<_i425.AuthRepository>()),
     );
